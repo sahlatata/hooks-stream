29a99e03e3ca1da38e7e8b752ece3e5ad355c977
sahlatata <sahlatata@gmail.com>
2022-02-18 18:32:11 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MovieList from './components/MovieList';
+import NavBar from './components/NavBar';
+function App() {
+ return (
+  <div >  
+    <NavBar/>
+  <MovieList/>
+  </div>
+  );
+}
+export default App;
